d3fcb89aa4cb8b9d0c8cc2f2347280b561f2891e
devidesai2011 <devidesai2011@gmail.com>
2025-10-02 17:31:20 +0530

    Build Maincomponent

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,7 +6,6 @@ const MainContainer = () => {
 
     const movieData = useSelector((store) => store?.movies?.nowPlayingMovies);
     const mainMovie = movieData ? movieData[0] : null;
-    // console.log(mainMovie);
     if (!mainMovie) return;
 
     const { original_title, overview, id } = mainMovie;
